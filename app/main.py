5ea6a069fec68c943e5182374e800d532f08d6b8
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2021-07-08 09:37:59 +0000

    add get_user function/jwt token verification

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
 from .db import users, todos
 from passlib.hash import bcrypt
 from pydantic import BaseModel
-from jose import jwt
+from jose import jwt, JWTError
 from dotenv import dotenv_values
 
 ALGORITHM = "HS256"
@@ -18,6 +20,19 @@ class Credentials(BaseModel):
 async def root():
     return {"message": "Hello World"}
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+
+async def get_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, config["SECRET_KEY"], algorithms=ALGORITHM)
+
+        return payload
+    except JWTError:
+        raise HTTPException(
+            status_code=401,
+            detail="Not authenticated"
+        )
+
 @app.post("/login")
 async def login(credentials: Credentials, response: Response):
     # users.getBy returns list of matching users
@@ -25,13 +40,13 @@ async def login(credentials: Credentials, response: Response):
 
     if len(user) == 0:
         response.status_code = 403
-        return {"error": "bad username or password"}
+        return {"detail": "Bad username or password"}
 
     user = user[0]
 
     if not bcrypt.verify(credentials.password, user["password"]):
         response.status_code = 403
-        return {"error": "bad username or password"}
+        return {"detail": "Bad username or password"}
 
     token = jwt.encode({"name": user["name"], "id": user["id"]}, config["SECRET_KEY"], algorithm=ALGORITHM)
 

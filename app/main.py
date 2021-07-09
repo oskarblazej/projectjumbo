52a69e5c27544f83a97c681aacb3a3bef90782c0
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2021-07-09 12:23:20 +0000

    enable CORS

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 
 from .auth import auth_router
 from .todos import todos_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(todos_router)
